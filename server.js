740619ed34c05447a8f955c25c0d6fbc874e4b41
Vaibhavkumar-tech <vaibhavkumar62078@example.com>
2026-04-20 16:04:04 +0530

    link update

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const app=require("./src/app")
-const connectionDB=require("./src/db/db")
+const app = require("./src/app");
+const connectionDB = require("./src/db/db");
 const cors = require("cors");
-app.use(cors());
 
+app.use(cors());
 
 connectionDB();
 
-app.listen(3000,()=>{
-    console.log("connect to server 3000")
-})
+if (process.env.NODE_ENV !== "production") {
+  app.listen(3000, () => {
+    console.log("connect to server 3000");
+  });
+}
+
+module.exports = app;
